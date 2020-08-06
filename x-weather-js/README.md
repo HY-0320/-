2ce2b4d1014060b45aed73d5c3fb6d795414af46
HY-0320 <418817150@qq.com>
2020-08-06 11:02:25 +0800

    2

---
+++ x-weather-js/README.md
@@ -0,0 +1,4 @@
+# X-weather
+X-weather
+前端：hy xyw
+后端：tbc ml
---
+++ x-weather-js/pages/details/details.js
@@ -0,0 +1,74 @@
+// pages/details/details.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    week:[
+      {Mon:"周一"},
+      {Tues:"周二"},
+      {Wed:"周三"},
+      {Thur:"周四"},
+      {Fri:"周五"},
+      {Sat:"周六"},
+      {Sun:"周日"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
